9ae16b48f1e4583e49f8048d44a29751bc73f2c1
hanifahfanidyautari <45959864+hanifahfanidyautari@users.noreply.github.com>
2019-01-15 08:27:36 +0700

    header up

--- application/views/header.php
+++ application/views/header.php
@@ -41,7 +41,7 @@
     <!--header start-->
     <header class="header black-bg">
       <div class="sidebar-toggle-box">
-        <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+        <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Min/Max Navigation"></div>
       </div>
       <!--logo start-->
       <a href=<?php echo site_url('Welcome/');?> class="logo"><b>DIN<span>KES</span></b></a>
