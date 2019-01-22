c16b6b2f92db3b4c4b9c010c2938e6463fcda9ec
hanifahfanidyautari <45959864+hanifahfanidyautari@users.noreply.github.com>
2019-01-22 10:58:46 +0700

    jgkgk

--- application/views/previewcetak.php
+++ application/views/previewcetak.php
@@ -28,10 +28,12 @@
 
 <body>
 
-<p style="text-align: center">Laporan blablabla</p>
+
+<h1>
+<p style="text-align: center">Laporan Bulanan Kesga</h1></p>
 <table>
 	<tr>
-		<th></th>
+		<th>Nama Laporan</th>
 	
 		<th>Batu</th>
 		<th>Junrejo</th>
