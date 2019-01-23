3ad74a4970ef10651ceac3ccdb1c512ff80b776b
hanifahfanidyautari <45959864+hanifahfanidyautari@users.noreply.github.com>
2019-01-23 14:53:29 +0700

    login bg2

--- application/views/login.php
+++ application/views/login.php
@@ -94,7 +94,7 @@
   <!-- You can use an image of whatever size. This script will stretch to fit in any screen size.-->
   <script type="text/javascript" src="<?php echo base_url()?>/assets/lib/jquery.backstretch.min.js"></script>
   <script>
-    $.backstretch("<?php echo base_url()?>/assets/img/login-bg.jpg", {
+    $.backstretch("<?php echo base_url()?>/assets/img/login-bg2.jpg", {
       speed: 500
     });
   </script>
